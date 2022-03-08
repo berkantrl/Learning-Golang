34e01ac771b2a6bbeb67ada9a488a455047e51c8
Berkan Türel <65579418+berkantrl@users.noreply.github.com>
2022-03-08 15:57:09 +0300

    Update hello.go

--- hello.go
+++ hello.go
@@ -1,51 +1,51 @@
 package main
 
-//import "fmt"
+import "fmt"
 
 // harf büyük olursa public oluyor küçük harfle başlıyorsa private func oluyor
 func main() {
-	// fmt.Println("Hello Security !")
+	 fmt.Println("Hello Security !")
 
-	// var i int
-	// i = 42
-	// println(i)
+	 var i int
+	 i = 42
+	 println(i)
 
-	// var f float32 = 3.14
-	// println(f)
+	 var f float32 = 3.14
+	 println(f)
 
-	// name := "Berkan"
-	// print(name)
+	 name := "Berkan"
+	 print(name)
 
-	// println(i, f, name)
+	 println(i, f, name)
 
-	// var lastname *string = new(string)
-	// *lastname = "Türel"
+	 var lastname *string = new(string)
+	 *lastname = "Türel"
 
-	// fmt.Println(lastname, *lastname)
+	 fmt.Println(lastname, *lastname)
 
-	// var pname *string = &name
+	 var pname *string = &name
 
-	// *pname = "Yousuf"
+	 *pname = "Yousuf"
 
-	// println(name)
+	 println(name)
 
-	// const age int = 20
+	 const age int = 20
 
-	// println(age)
+	 println(age)
 
-	// println(first, second, thirty)
-	// println(monday, tday, wday)
+	 println(first, second, thirty)
+	 println(monday, tday, wday)
 
 }
 
-// const (
-// 	first = iota
-// 	second
-// 	thirty
-// )
+const (
+ 	first = iota
+	second
+ 	thirty
+)
 
-// const (
-// 	monday = iota + 1
-// 	tday   = 2 << iota
-// 	wday
-// )
+const (
+	monday = iota + 1
+ 	tday   = 2 << iota
+	wday
+)
